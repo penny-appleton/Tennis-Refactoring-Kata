aae554e580c8f6bc5addea6ddbcb14ff9746b53f
pappleton <penny@industriallogic.com>
2021-05-03 13:17:40 -0500

    had constructor of TennisGame1 set initial value of score

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -5,11 +5,12 @@ public class TennisGame1 implements TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
-    private String initialScore = new All().setScore();
+    private String score;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        score = new All().setScore();
     }
 
     public void wonPoint(String playerName) {
