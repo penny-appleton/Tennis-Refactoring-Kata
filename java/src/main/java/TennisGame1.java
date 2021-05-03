81dca1098ec78ef3e698a8bccb885786cf228da3
pappleton <penny@industriallogic.com>
2021-05-03 14:27:08 -0500

    removed unnecessary field score

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -5,14 +5,12 @@ public class TennisGame1 implements TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
-    private String score;
     private Score state;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
         state = new All();
-        score = state.setScore(m_score1, m_score2);
     }
 
     public void wonPoint(String playerName) {
@@ -22,16 +20,15 @@ public class TennisGame1 implements TennisGame {
             m_score2 += 1;
     }
 
-    //Code smells:  Switch Statements, temp field
     public String getScore() {
         if (m_score1 == m_score2) {
-            return score = state.setScore(m_score1, m_score2);
+            return state.setScore(m_score1, m_score2);
         } else if (m_score1 >= 4 || m_score2 >= 4) {
             state = new Advantage();
-            return score = state.setScore(m_score1, m_score2);
+            return state.setScore(m_score1, m_score2);
         } else {
             state = new Normal();
-            return score = state.setScore(m_score1, m_score2);
+            return state.setScore(m_score1, m_score2);
         }
 
     }
